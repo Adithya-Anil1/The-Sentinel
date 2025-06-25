b6fa093a980b3a41ce60041083915f479ad815d8
Adithya <adithyaanil2004@gmail.com>
2025-06-25 14:24:41 +0530

    created categories

---
+++ backend/sources.js
@@ -0,0 +1,30 @@
+const sources = {
+  'Technology & Science': [
+    'https://feeds.feedburner.com/TechCrunch',
+    'https://www.wired.com/feed/rss',
+    'https://feeds.feedburner.com/oreilly/radar',
+    'https://www.sciencedaily.com/rss/top.xml'
+  ],
+  'World News': [
+    'http://feeds.bbci.co.uk/news/world/rss.xml',
+    'https://feeds.reuters.com/reuters/worldNews'
+  ],
+  'Business': [
+    'https://feeds.reuters.com/reuters/businessNews',
+    'https://feeds.feedburner.com/entrepreneur/latest'
+  ],
+  'Politics': [
+    'https://feeds.reuters.com/reuters/politicsNews',
+    'https://feeds.feedburner.com/politico/fAVF'
+  ],
+  'Weather': [
+    'https://feeds.weather.gov/us/us.xml',
+    'https://www.weather.com/rss/weather/today'
+  ],
+  'Stocks': [
+    'https://feeds.finance.yahoo.com/rss/2.0/topfinstories',
+    'https://feeds.reuters.com/reuters/markets'
+  ]
+};
+
+module.exports = sources;
